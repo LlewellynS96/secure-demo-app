98e88cc07bf41da642220ee2c891b40f2f7f8492
Llewellyn Strydom <llewellyn.strydom@saab.ae>
2023-09-21 16:14:28 +0400

    Add registry to install script

--- install.sh
+++ install.sh
@@ -3,19 +3,22 @@
 CLUSTER_NAME=secure-demo-app
 NEW_CLUSTER=false
 UNINSTALL=false
+CREATE_REGISTRY=false
 EXTERNAL_LB=false
 ISTIO=false
 DISABLE_LB=""
 INSTALL_MONITORING=false
 INSTALL_BOOKINFO=false
 
-while getopts ":nc:ulimb" option; do
+while getopts ":nc:urlimb" option; do
    case $option in
       n) NEW_CLUSTER=true
          ;;
       c) CLUSTER_NAME=$OPTARG
          ;;
       u) UNINSTALL=true
+         ;;
+      r) CREATE_REGISTRY=true
          ;;
       l) EXTERNAL_LB=true
          ;;
@@ -32,15 +35,28 @@ while getopts ":nc:ulimb" option; do
 done
 
 if $UNINSTALL ; then
-   k3d cluster delete $CLUSTER_NAME
+  k3d registry delete $CLUSTER_NAME-registry
+  k3d cluster delete $CLUSTER_NAME
 fi
 
 if $EXTERNAL_LB ; then
-   DISABLE_LB='--k3s-arg --disable=servicelb@server:*'
+  DISABLE_LB='--k3s-arg --disable=servicelb@server:*'
+fi
+
+USE_REGISTRY=""
+
+if $CREATE_REGISTRY ; then
+  if [ ! -z "$(docker network ls | grep k3d-$CLUSTER_NAME)" ]; then
+    docker network create k3d-$CLUSTER_NAME
+  else
+  echo 5
+  fi
+  k3d registry create $CLUSTER_NAME-registry --default-network k3d-$CLUSTER_NAME --port 12345
+  USE_REGISTRY="--registry-use k3d-$CLUSTER_NAME-registry"
 fi
 
 if $NEW_CLUSTER ; then
-  k3d cluster create $CLUSTER_NAME --k3s-arg "--disable=traefik@server:*" $DISABLE_LB --no-lb -p "30000-30099:30000-30099@server:0:direct"
+  k3d cluster create $CLUSTER_NAME --k3s-arg "--disable=traefik@server:*" $DISABLE_LB $USE_REGISTRY --no-lb -p "30000-30099:30000-30099@server:0:direct"
 fi
 
 if $EXTERNAL_LB ; then
